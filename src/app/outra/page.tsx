2d33aaadb354f60baae1d6e381f45bf13e15bd43
Evaldo Souza <evaldo.souza2@gmail.com>
2025-04-16 12:48:10 -0300

    deixando mais claro se tratar de um teste

--- src/app/outra/page.tsx
+++ src/app/outra/page.tsx
@@ -1,3 +1,7 @@
 export default function Outra() {
-  return <h1>Outra pagina para teste</h1>;
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>Outra pagina do RIASEC</h1>
+    </div>
+  );
 }
