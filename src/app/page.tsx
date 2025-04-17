76be6879a541c037dd40650ff1796c7de51d3928
Evaldo Souza <evaldo.souza2@gmail.com>
2025-04-17 11:38:43 -0300

    Nit picks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       Pagina inicial do RIASEC360.
-      <Link href="/outra">Link para uma subpagina</Link>
+      <Link href={"/outra"}>Link para uma subpagina</Link>
     </div>
   );
 }
