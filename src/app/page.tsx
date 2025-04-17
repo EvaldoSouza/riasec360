cdbc36997acf0e8c75ae2614d725e20640a97867
Evaldo Souza <evaldo.souza2@gmail.com>
2025-04-17 13:51:42 -0300

    Link para voltar a pagina principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       Pagina inicial do RIASEC360.
       <Link href={"/outra"}>Link para uma subpagina</Link>
+      <Link href={"http://si3cloud.ufsj.edu.br/"}>
+        Voltar para a pagina Principal
+      </Link>
     </div>
   );
 }
